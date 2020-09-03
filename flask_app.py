ed89f57588dffd6076edce282ac9c3e543246a31
贤二 <yuanxun@gmail.com>
2020-09-03 17:31:51 +0800

    feat (service-layer) do the error handle

--- flask_app.py
+++ flask_app.py
@@ -21,6 +21,10 @@ get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
 
 
+def is_valid_sku(sku, batches):
+    return sku in {b.sku for b in batches}
+
+
 @app.route("/allocate", methods=["POST"])
 def allocate_endpoint():
     session = get_session()
@@ -31,6 +35,14 @@ def allocate_endpoint():
         , request.json['qty']
     )
 
-    batchref = model.allocate(line, batches)
+    if not is_valid_sku(line.sku, batches):
+        return jsonify({'message': f'Invalid sku {line.sku}'}), 400
+
+    try:
+        batchref = model.allocate(line, batches)
+    except model.OutOfStock as e:
+        return jsonify({'message': str(e)}), 400
+
+    session.commit()
 
     return jsonify({'batchref': batchref}), 201
