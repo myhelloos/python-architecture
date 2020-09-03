aad3891f12d93b04a4030e842be40eb4aa1ab432
贤二 <yuanxun@gmail.com>
2020-09-03 13:00:52 +0800

    feat (service layer) first api test

---
+++ config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: alfred
+@license: (C) Copyright 2019-2020, Alfred Yuan Limited.
+@file: config.py
+@time: 2020/9/3 11:10 AM
+@desc:
+"""
+import os
+
+
+def get_postgres_uri():
+    host = os.environ.get('DB_HOST', 'localhost')
+    port = 5432 if host == 'localhost' else 5432
+    password = os.environ.get('DB_PASSWORD', 'allocation')
+    user, db_name = 'allocation', 'allocation'
+    return f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
+
+
+def get_api_url():
+    host = os.environ.get('API_HOST', 'localhost')
+    port = 5005 if host == 'localhost' else 80
+    return f'http://{host}:{port}'
---
+++ test_api.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: alfred
+@license: (C) Copyright 2019-2020, Alfred Yuan Limited.
+@file: test_api.py
+@time: 2020/9/3 11:35 AM
+@desc:
+"""
+import uuid
+
+import pytest
+import requests
+
+import config
+
+
+def random_suffix():
+    return uuid.uuid4().hex[:6]
+
+
+def random_sku(name=''):
+    return f'sku-{name}-{random_suffix()}'
+
+
+def random_batchref(name=''):
+    return f'batch-{name}-{random_suffix()}'
+
+
+def random_orderid(name=''):
+    return f'order-{name}-{random_suffix()}'
+
+
+@pytest.mark.usefixtures('restart_api')
+def test_api_returns_allocation(add_stock):
+    sku, other_sku = random_sku(), random_sku('other')
+    early_batch = random_batchref(1)
+    later_batch = random_batchref(2)
+    other_batch = random_batchref(3)
+
+    add_stock([
+        (later_batch, sku, 100, '2011-01-02')
+        , (early_batch, sku, 100, '2011-01-01')
+        , (other_batch, other_sku, 100, None)])
+
+    data = {'orderid': random_orderid(), 'sku': sku, 'qty': 3}
+    url = config.get_api_url()
+    r = requests.post(f'{url}/allocate', json=data)
+
+    assert r.status_code == 201
+    assert r.json()['batchref'] == early_batch
