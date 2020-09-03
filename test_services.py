37aba6451d2db6f0ebc72418ff3d0dfd1e41134c
贤二 <yuanxun@gmail.com>
2020-09-03 18:30:16 +0800

    feat (service-layer) fakse session

--- test_services.py
+++ test_services.py
@@ -28,6 +28,13 @@ class FakeRepository(repository.AbstractRepository):
         return list(self.__batches)
 
 
+class FakeSession():
+    commited = False
+
+    def commit(self):
+        self.commited = True
+
+
 def test_returns_allocation():
     batch = model.Batch('b1', 'COMPLICATED-LAMP', 100, eta=None)
     repo = FakeRepository([batch])
@@ -36,6 +43,7 @@ def test_returns_allocation():
     result = services.allocate(line, repo, FakeSession())
 
     assert result == 'b1'
+
 
 def test_error_for_invalid_sku():
     batch = model.Batch('b1', 'AREALSKU', 100, eta=None)
