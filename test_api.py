298c72ee9da0b9966ca7643409c1a2f1f063271e
贤二 <yuanxun@gmail.com>
2020-09-04 11:33:47 +0800

    feat (service-layer) strip out unnecessary e2e tests

--- test_api.py
+++ test_api.py
@@ -32,7 +32,7 @@ def random_orderid(name=''):
 
 
 @pytest.mark.usefixtures('restart_api')
-def test_api_returns_allocation(add_stock):
+def test_happy_path_returns_201_and_allocated_batch(add_stock):
     sku, other_sku = random_sku(), random_sku('other')
     early_batch = random_batchref(1)
     later_batch = random_batchref(2)
@@ -53,50 +53,10 @@ def test_api_returns_allocation(add_stock):
 
 
 @pytest.mark.usefixtures('restart_api')
-def test_allocations_are_persisted(add_stock):
-    sku = random_sku()
-    batch_1, batch_2 = random_batchref(1), random_batchref(2)
-    order_1, order_2 = random_orderid(1), random_orderid(2)
+def test_unhappy_path_returns_400_and_error_message():
+    unknown_sku, order_id = random_sku(), random_orderid()
 
-    add_stock([
-        (batch_1, sku, 10, '2011-01-01')
-        , (batch_2, sku, 10, '2011-01-02')
-    ])
-
-    line_1 = {'orderid': order_1, 'sku': sku, 'qty': 10}
-    line_2 = {'orderid': order_2, 'sku': sku, 'qty': 10}
-
-    url = config.get_api_url()
-
-    r = requests.post(f'{url}/allocate', json=line_1)
-    assert r.status_code == 201
-    assert r.json()['batchref'] == batch_1
-
-    r = requests.post(f'{url}/allocate', json=line_2)
-    assert r.status_code == 201
-    assert r.json()['batchref'] == batch_2
-
-
-@pytest.mark.usefixtures('restart_api')
-def test_400_message_for_out_of_stock(add_stock):
-    sku, small_batch, large_order = random_sku(), random_batchref(), random_orderid()
-    add_stock([
-        (small_batch, sku, 10, '2011-01-01')
-    ])
-
-    data = {'orderid': large_order, 'sku': sku, 'qty': 20}
-    url = config.get_api_url()
-    r = requests.post(f'{url}/allocate', json=data)
-
-    assert r.status_code == 400
-    assert r.json()['message'] == f'Out of stock for sku {sku}'
-
-
-@pytest.mark.usefixtures('restart_api')
-def test_400_message_for_invalid_sku():
-    unknown_sku, orderid = random_sku(), random_orderid()
-
-    data = {'orderid': orderid, 'sku': unknown_sku, 'qty': 10}
+    data = {'orderid': order_id, 'sku': unknown_sku, 'qty': 10}
     url = config.get_api_url()
     r = requests.post(f'{url}/allocate', json=data)
 
