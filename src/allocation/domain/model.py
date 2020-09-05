d6c62e17e13367020a6cac68679995af04e550b4
alfred <yuanxun.cn@gmail.com>
2020-09-05 18:08:12 +0800

    feat (uow) mypy works

--- src/allocation/domain/model.py
+++ src/allocation/domain/model.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from datetime import date
-from typing import Optional, List
+from typing import Optional, List, Set
 
 
 def allocate(line: OrderLine, batches: List[Batch]) -> str:
